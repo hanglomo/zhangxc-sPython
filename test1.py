33ea6d7989091d7e6d0d99f23c8405f87de3c61e
hanglomo <shhlomo@Outlook.com>
2021-03-19 14:50:52 +0800

    Add files via upload
    张小成Python第一课（初识Python）记录

---
+++ test1.py
@@ -0,0 +1,12 @@
+    print("Hello Wordr")
+    print("我叫张小成")
+    print("   *")
+    print("  ***")
+    print(" *****")
+    print("*******")
+    N=100
+    print(N)
+    input("请输入：")
+    n=int(input("请输入海绵宝宝的年龄："))
+    m=int(input("请输入派大星的年龄："))
+    print("他们的年龄和为：",n+m)
