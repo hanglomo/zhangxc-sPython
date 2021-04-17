c692d0be86cd7dd2183e892f16204e5c26649429
hanglomo <shhlomo@Outlook.com>
2021-04-17 11:36:56 +0800

    Create class5-test1.py
    张小成课堂代码

---
+++ class5-test1.py
@@ -0,0 +1,18 @@
+    #第二条是删除键
+    apple=(0,1,2,3,4,5,6,7,8,9)
+    del apple
+    print(apple)
+    list(apple)
+    tuple(apple)
+    #############################元组
+    abc={4,5,7,32}
+    print(abc)
+    for i in abc:
+        print(abc)
+
+    #增加
+    XXX.add(数据)
+    #移除
+    XXX.remove(数据)
+    #清空
+    XXX.clear(数据)
